19871d1a86fe9421530563f02b52d4d7378c716c
PatrikOndrus <patrik.ondrus@iteria.io>
2022-04-22 07:13:35 +0200

    c13

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import {
 } from "react-devtools-inline/backend";
 import {
   createBridge as createFrontendBridge,
-  // createStore,
+  createStore,
   initialize as createDevTools,
   //@ts-ignore
 } from "react-devtools-inline/frontend";
-import { Store } from "./bridgeOperations";
+// import { Store } from "./bridgeOperations";
 
-import { showDevtoolsWarning } from "./warning";
+// import { showDevtoolsWarning } from "./warning";
 declare global {
   interface Window {
     __REACT_DEVTOOLS_GLOBAL_HOOK__: any;
@@ -20,16 +20,41 @@ declare global {
   }
 }
 
-if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__?.renderers.size === 0) {
-  console.log("delete devtools", window.__REACT_DEVTOOLS_GLOBAL_HOOK__);
-  delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
-} else if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__?.renderers.size > 0) {
-  console.log("show warning devtools", window.__REACT_DEVTOOLS_GLOBAL_HOOK__);
-  showDevtoolsWarning();
-}
+// if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__?.renderers.size === 0) {
+//   console.log("delete devtools", window.__REACT_DEVTOOLS_GLOBAL_HOOK__);
+//   delete window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+// } else if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__?.renderers.size > 0) {
+//   console.log("show warning devtools", window.__REACT_DEVTOOLS_GLOBAL_HOOK__);
+//   showDevtoolsWarning();
+// }
 
-const store = new Store();
-window.addEventListener("message", ({ data }) => {
+// const store = new Store()
+// window.addEventListener('message', ({ data }) => {
+//   if (data.event === 'operations') {
+//     store.onBridgeOperations(data.payload)
+//     const devtoolsTree = store._idToElement
+//     //@ts-ignore
+//     window.devtoolsTree = devtoolsTree
+//   }
+// })
+
+const wall = {
+  //@ts-ignore
+  listen(listener) {
+    window.addEventListener("message", (event) => {
+      listener(event.data);
+    });
+  },
+  //@ts-ignore
+  send(event, payload) {
+    window.parent.postMessage({ event, payload }, "*");
+  },
+};
+
+const frontBridge = createFrontendBridge(window, wall);
+const store = createStore(frontBridge);
+//@ts-ignore
+wall.listen((data) => {
   if (data.event === "operations") {
     store.onBridgeOperations(data.payload);
     const devtoolsTree = store._idToElement;
@@ -38,33 +63,6 @@ window.addEventListener("message", ({ data }) => {
   }
 });
 
-// const wall = {
-//   //@ts-ignore
-//   listen(listener) {
-//     window.addEventListener("message", ({ data }) => {
-//       listener(data);
-//     });
-//   },
-//   //@ts-ignore
-//   send(event, payload) {
-//     window.parent.postMessage({ event, payload }, "*");
-//   },
-// };
-
-const rootElement = document.getElementById("root")!;
-const frontBridge = createFrontendBridge(rootElement);
-
-// const store: Store = createStore(frontBridge);
-// //@ts-ignore
-// wall.listen((data) => {
-//   if (data.event === "operations") {
-//     store.onBridgeOperations(data.payload);
-//     const devtoolsTree = store._idToElement;
-//     //@ts-ignore
-//     window.devtoolsTree = devtoolsTree;
-//   }
-// });
-
 window.__FRONTEND_DEVTOOLS__ = createDevTools(window, {
   bridge: frontBridge,
   store,
@@ -72,5 +70,5 @@ window.__FRONTEND_DEVTOOLS__ = createDevTools(window, {
 
 initializeBackend(window);
 activateBackend(window, {
-  bridge: createBackendBridge(window),
+  bridge: createBackendBridge(window, wall),
 });
